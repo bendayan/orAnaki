b8d2482ac7c054475443d517792381631f6a856a
Shem Bar <shem@svcollege.co.il>
2018-07-02 20:43:47 +0300

    firstExample

---
+++ myJavaScript.js
@@ -0,0 +1,100 @@
+{
+   var n1=6 , n2=7 , n3 , n4 , firstName , lastName;
+   n3 = 70;
+   n4 = 135;
+   firstName = "shem";
+   lastName = "bar";
+
+   n4 = 67;
+
+   var res = n1*n2*n3*n4;
+   if(res>1000000){
+       alert(":)");
+   }
+   else if(res==1000000){
+       alert(":|");
+   }
+   else {
+       alert(":(");
+   }
+   alert("num3");
+
+
+   document.getElementById("fullName").innerHTML=firstName+ " "+ lastName;
+
+   var res2=n4-n1;
+
+   if(res2 % 2 == 0){
+       alert("EVEN");
+   }
+   else{
+       alert("ODD");
+   }
+
+   num3++; 
+   /*num3+=1;
+   num3=num3+1;
+*/
+
+   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bla(){
+//     d=document.getElementById("demo").value;
+//     if(d>150){
+//         alert("noooooooo")
+//     }
+//     else{
+//         alert("yaaaaaaaa");
+//     }
+// }
+
+
+
+
+
+//}
+
+
+
